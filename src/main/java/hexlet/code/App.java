8f8fa9e9695480ab678ece78b932beed428ba8a7
Alexander L <12oprs@gmail.com>
2021-10-15 09:58:09 +1200

    add picocli help

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,42 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+         description = "Compares two configuration files and shows a difference.")
+
+class GenDiff implements Callable<Integer> {
+
+    @Parameters(index = "0", description = "path to first file")
+    private String filepath1;
+
+    @Parameters(index = "1", description = "path to second file")
+    private String filepath2;
+
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    private String format = "stylish";
+
+    @Override
+    public Integer call() throws Exception { 
+
+        return 0;
+    }
+}
+
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+
+		int exitCode = new CommandLine(new GenDiff()).execute(args);
+        System.exit(exitCode);
+
 	}
 }
