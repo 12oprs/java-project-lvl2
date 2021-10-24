5b2e1ff3921295c42b70314a7c75eb01b00e8a95
Alexander L <12oprs@gmail.com>
2021-10-24 19:43:08 +1200

    add test plain format

--- src/test/java/hexlet/code/AppTest.java
+++ src/test/java/hexlet/code/AppTest.java
@@ -16,43 +16,63 @@ class AppTest {
     private static final String FILE2 = "file2.json";
     private static final String FILE3 = "file1.yml";
     private static final String FILE4 = "file2.yml";
-    private static final String FILE5 = "resultDiff";
-    private static final String FORMAT = "stylish";
+    private static final String RESULT1 = "resultDiff";
+    private static final String RESULT2 = "resultPlainDiff";
+    // private static final String FORMAT = "stylish";
 
-    private static String filePath1;
-    private static String filePath2;
-    private static String filePath3;
-    private static String filePath4;
-    private static String filePath5;
+    // private static String filePath1;
+    // private static String filePath2;
+    // private static String filePath3;
+    // private static String filePath4;
+    // private static String filePath5;
     private static String expected;
+    private static String expectedPlain;
 
     @BeforeAll
     static void preparing() throws Exception {
-        filePath1 = Paths.get(RESOURCE_DIRECTORY, FILE1).toFile().getAbsolutePath();
-        filePath2 = Paths.get(RESOURCE_DIRECTORY, FILE2).toFile().getAbsolutePath();
-        filePath3 = Paths.get(RESOURCE_DIRECTORY, FILE3).toFile().getAbsolutePath();
-        filePath4 = Paths.get(RESOURCE_DIRECTORY, FILE4).toFile().getAbsolutePath();
-        filePath5 = Paths.get(RESOURCE_DIRECTORY, FILE5).toFile().getAbsolutePath();
-        String[] temp = Files.lines(Path.of(filePath5))
+        String pathToResult = Paths.get(RESOURCE_DIRECTORY, RESULT1).toFile().getAbsolutePath();
+        String[] temp1 = Files.lines(Path.of(pathToResult))
                 .toArray(String[]::new);
         StringBuilder sb = new StringBuilder();
-        for (String s : temp) {
+        for (String s : temp1) {
             sb.append(s).append("\n");
         }
         expected = sb.toString();
+
+        String pathToPlainResult = Paths.get(RESOURCE_DIRECTORY, RESULT2).toFile().getAbsolutePath();
+        String[] temp2 = Files.lines(Path.of(pathToPlainResult))
+                .toArray(String[]::new);
+        sb = new StringBuilder();
+        for (String s : temp2) {
+            sb.append(s).append("\n");
+        }
+        expectedPlain = sb.toString();
     }
 
     @Test
     @DisplayName("Run app with json testfiles")
     void jsonTest() throws Exception {
-        Differ testDf = new Differ(filePath1, filePath2, FORMAT);
+        String filePath1 = Paths.get(RESOURCE_DIRECTORY, FILE1).toFile().getAbsolutePath();
+        String filePath2 = Paths.get(RESOURCE_DIRECTORY, FILE2).toFile().getAbsolutePath();
+        Differ testDf = new Differ(filePath1, filePath2, "stylish");
         assertEquals(expected, testDf.generate());
     }
 
     @Test
     @DisplayName("Run app with yaml testfiles")
     void yamlTest() throws Exception {
-        Differ testDf = new Differ(filePath3, filePath4, FORMAT);
+        String filePath3 = Paths.get(RESOURCE_DIRECTORY, FILE3).toFile().getAbsolutePath();
+        String filePath4 = Paths.get(RESOURCE_DIRECTORY, FILE4).toFile().getAbsolutePath();
+        Differ testDf = new Differ(filePath3, filePath4, "stylish");
         assertEquals(expected, testDf.generate());
+    }
+
+    @Test
+    @DisplayName("Run app with plain formatter")
+    void formatterPlainTest() throws Exception {
+        String filePath1 = Paths.get(RESOURCE_DIRECTORY, FILE1).toFile().getAbsolutePath();
+        String filePath2 = Paths.get(RESOURCE_DIRECTORY, FILE2).toFile().getAbsolutePath();
+        Differ testDf = new Differ(filePath1, filePath2, "plain");
+        assertEquals(expectedPlain, testDf.generate());
     }
 }
