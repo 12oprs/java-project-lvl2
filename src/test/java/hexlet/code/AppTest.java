a5c9886fac8434f3515a20a57ded921cbd3f6864
Alexander L <12oprs@gmail.com>
2021-10-28 18:23:33 +1200

    add json formatter test

--- src/test/java/hexlet/code/AppTest.java
+++ src/test/java/hexlet/code/AppTest.java
@@ -18,15 +18,11 @@ class AppTest {
     private static final String FILE4 = "file2.yml";
     private static final String RESULT1 = "resultDiff";
     private static final String RESULT2 = "resultPlainDiff";
-    // private static final String FORMAT = "stylish";
+    private static final String RESULT3 = "resultJsonDiff";
 
-    // private static String filePath1;
-    // private static String filePath2;
-    // private static String filePath3;
-    // private static String filePath4;
-    // private static String filePath5;
     private static String expected;
     private static String expectedPlain;
+    private static String expectedJson;
 
     @BeforeAll
     static void preparing() throws Exception {
@@ -42,11 +38,20 @@ class AppTest {
         String pathToPlainResult = Paths.get(RESOURCE_DIRECTORY, RESULT2).toFile().getAbsolutePath();
         String[] temp2 = Files.lines(Path.of(pathToPlainResult))
                 .toArray(String[]::new);
-        sb = new StringBuilder();
+        sb = new StringBuilder();;
         for (String s : temp2) {
             sb.append(s).append("\n");
         }
         expectedPlain = sb.toString();
+
+        String pathToJsonResult = Paths.get(RESOURCE_DIRECTORY, RESULT3).toFile().getAbsolutePath();
+        String[] temp3 = Files.lines(Path.of(pathToJsonResult))
+                .toArray(String[]::new);
+        sb = new StringBuilder();;
+        for (String s : temp3) {
+            sb.append(s).append("\n");
+        }
+        expectedJson = sb.toString();
     }
 
     @Test
@@ -54,8 +59,8 @@ class AppTest {
     void jsonTest() throws Exception {
         String filePath1 = Paths.get(RESOURCE_DIRECTORY, FILE1).toFile().getAbsolutePath();
         String filePath2 = Paths.get(RESOURCE_DIRECTORY, FILE2).toFile().getAbsolutePath();
-        Differ testDf = new Differ(filePath1, filePath2, "stylish");
-        assertEquals(expected, testDf.generate());
+        Differ testDf = new Differ();
+        assertEquals(expected, testDf.generate(filePath1, filePath2, "stylish"));
     }
 
     @Test
@@ -63,8 +68,8 @@ class AppTest {
     void yamlTest() throws Exception {
         String filePath3 = Paths.get(RESOURCE_DIRECTORY, FILE3).toFile().getAbsolutePath();
         String filePath4 = Paths.get(RESOURCE_DIRECTORY, FILE4).toFile().getAbsolutePath();
-        Differ testDf = new Differ(filePath3, filePath4, "stylish");
-        assertEquals(expected, testDf.generate());
+        Differ testDf = new Differ();
+        assertEquals(expected, testDf.generate(filePath3, filePath4, "stylish"));
     }
 
     @Test
@@ -72,7 +77,15 @@ class AppTest {
     void formatterPlainTest() throws Exception {
         String filePath1 = Paths.get(RESOURCE_DIRECTORY, FILE1).toFile().getAbsolutePath();
         String filePath2 = Paths.get(RESOURCE_DIRECTORY, FILE2).toFile().getAbsolutePath();
-        Differ testDf = new Differ(filePath1, filePath2, "plain");
-        assertEquals(expectedPlain, testDf.generate());
+        Differ testDf = new Differ();
+        assertEquals(expectedPlain, testDf.generate(filePath1, filePath2, "plain"));
+    }
+    @Test
+    @DisplayName("Run app with json formatter")
+    void formatterJsonTest() throws Exception {
+        String filePath1 = Paths.get(RESOURCE_DIRECTORY, FILE1).toFile().getAbsolutePath();
+        String filePath2 = Paths.get(RESOURCE_DIRECTORY, FILE2).toFile().getAbsolutePath();
+        Differ testDf = new Differ();
+        assertEquals(expectedJson, testDf.generate(filePath1, filePath2, "json"));
     }
 }
