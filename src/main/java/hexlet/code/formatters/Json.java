788507797f1d8bbc8d6821cf2d63ec713ea32c4f
Alexander L <12oprs@gmail.com>
2021-11-17 15:01:37 +1200

    fix json formatter

--- src/main/java/hexlet/code/formatters/Json.java
+++ src/main/java/hexlet/code/formatters/Json.java
@@ -2,10 +2,15 @@ package hexlet.code.formatters;
 
 import java.util.Map;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.TreeMap;
 import java.util.Comparator;
 
 public class Json {
+
+    private static final String PATTERN =
+        "((?![-+]?[0-9]*\\.?[0-9]|null|true|false)\\b[^:=\\n\\t,]+\\b)";
 
     private static Map<String, Object> nochangedMap;
     private static Map<String, Object> addedMap;
@@ -15,17 +20,18 @@ public class Json {
         extractData(diff);
 
         StringBuilder sb = new StringBuilder();
-        sb.append("\t\"added\":{\n");
-        addedMap.forEach((k, v) -> sb.append("\t\t" + inBracesIfNeed(k) + ":" + inBracesIfNeed(v) + "\n"));
+        sb.append("\tadded:{\n");
+        addedMap.forEach((k, v) -> sb.append("\t\t" + k + ":" + v + "\n"));
         sb.append("\t}\n");
-        sb.append("\t\"nochanged\":{\n");
-        nochangedMap.forEach((k, v) -> sb.append("\t\t" + inBracesIfNeed(k) + ":" + inBracesIfNeed(v) + "\n"));
+        sb.append("\tnochanged:{\n");
+        nochangedMap.forEach((k, v) -> sb.append("\t\t" + k + ":" + v + "\n"));
         sb.append("\t}\n");
-        sb.append("\t\"removed\":{\n");
-        removedMap.forEach((k, v) -> sb.append("\t\t" + inBracesIfNeed(k) + ":" + inBracesIfNeed(v) + "\n"));
+        sb.append("\tremoved:{\n");
+        removedMap.forEach((k, v) -> sb.append("\t\t" + k + ":" + v + "\n"));
         sb.append("\t}\n");
         sb.insert(0, "{\n").append("}");
         String result = sb.toString();
+        result = strInBraces(result);
         return result;
     }
 
@@ -58,19 +64,15 @@ public class Json {
         }
     }
 
-
-    private static String inBracesIfNeed(final Object target) {
-        String result = target.toString();
-        if (!result.equals("null")
-                && !result.equals("true")
-                && !result.equals("false")) {
-            try {
-                double temp = Double.parseDouble(result);
-            } catch (NumberFormatException e) {
-                return "\"" + result + "\"";
-            }
-            return result;
+    private static String strInBraces(final String target) {
+        StringBuilder result = new StringBuilder();
+        Pattern p = Pattern.compile(PATTERN);
+        Matcher m = p.matcher(target);
+        while (m.find()) {
+            m.appendReplacement(result, "\"" + m.group() + "\"");
         }
-        return result;
+        m.appendTail(result);
+        return result.toString();
     }
+
 }
